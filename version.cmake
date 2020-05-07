0a98543a7ee179a40e7fe12fc38a34ea0a17d0a4
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2020-05-07 15:14:07 +0800

    add packaging via cpack

---
+++ version.cmake
@@ -0,0 +1,8 @@
+#...............................................................................
+
+set(PLH_VERSION_MAJOR 1)
+set(PLH_VERSION_MINOR 0)
+set(PLH_VERSION_PATCH 0)
+set(PLH_VERSION_FULL  "${PLH_VERSION_MAJOR}.${PLH_VERSION_MINOR}.${PLH_VERSION_PATCH}")
+
+#...............................................................................
