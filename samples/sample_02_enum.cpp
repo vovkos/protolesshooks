59e9187028dd1506ade6b176fd1e25f9ff903a99
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2020-05-19 16:11:07 +0800

    typo fix in sample_02_enum

--- samples/sample_02_enum.cpp
+++ samples/sample_02_enum.cpp
@@ -19,9 +19,9 @@ main()
 			if (importIt.getOrdinal() != -1)
 				printf("  %s@%d\n", importIt.getModuleName(), importIt.getOrdinal());
 			else
-				printf("  %s:%s\n", importIt.getModuleName(), it.getSymbolName());
+				printf("  %s:%s\n", importIt.getModuleName(), importIt.getSymbolName());
 	#elif (_PLH_OS_LINUX)
-			printf("  %s\n", it.getSymbolName());
+			printf("  %s\n", importIt.getSymbolName());
 	#elif (_PLH_OS_DARWIN)
 			printf(
 				"  %s:%s...\n",
