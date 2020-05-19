0ea5881009c2fb2b00c36194f1765abf4a3d3a63
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2020-05-19 17:53:44 +0800

    code-style fix in sample_02_enum

--- samples/sample_02_enum.cpp
+++ samples/sample_02_enum.cpp
@@ -15,20 +15,20 @@ main()
 		plh::ImportIterator importIt = plh::enumerateImports(moduleIt);
 		for (; importIt; importIt++)
 		{
-	#if (_PLH_OS_WIN)
+#if (_PLH_OS_WIN)
 			if (importIt.getOrdinal() != -1)
 				printf("  %s@%d\n", importIt.getModuleName(), importIt.getOrdinal());
 			else
 				printf("  %s:%s\n", importIt.getModuleName(), importIt.getSymbolName());
-	#elif (_PLH_OS_LINUX)
+#elif (_PLH_OS_LINUX)
 			printf("  %s\n", importIt.getSymbolName());
-	#elif (_PLH_OS_DARWIN)
+#elif (_PLH_OS_DARWIN)
 			printf(
 				"  %s:%s...\n",
 				importIt.getModuleName(),
 				importIt.getSymbolName()
 				);
-	#endif
+#endif
 		}
 	}
 
